3de7f1276c0894a55211b382122370c3c969344b
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-23 17:52:03 +0530

    Update colour_code_reference_manual.py

--- colour_code_reference_manual.py
+++ colour_code_reference_manual.py
@@ -1,11 +1,16 @@
-from color_code_Pairing import get_color_from_pair_number
+from color_code_Pairing import get_color_from_pair_number, color_pair_to_string, MAJOR_COLORS, MINOR_COLORS
 
-def print_reference_manual(): #Prints the color code reference manual.
-    print("Color Code Reference Manual")
-    print("---------------------------")
-    for pair_number in range(1, 26):
-        major_color, minor_color = get_color_from_pair_number(pair_number)
-        print(f"{pair_number:2}: {major_color:<6} - {minor_color:<6}")
+def generate_reference_manual():
+    reference_manual = []
+    reference_manual.append("Color Code Reference Manual\n")
+    reference_manual.append("----------------------------\n")
+    for i in range(1, len(MAJOR_COLORS) * len(MINOR_COLORS) + 1):
+        color_pair = get_color_from_pair_number(i)
+        color_pair_names = color_pair_to_string(color_pair)
+        line = f"{i}: {color_pair_names}\n"
+        reference_manual.append(line)
+    return ''.join(reference_manual)
 
 if __name__ == '__main__':
-    print_reference_manual()
+    manual = generate_reference_manual()
+    print(manual)
