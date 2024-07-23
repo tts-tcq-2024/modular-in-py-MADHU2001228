0cf6ce07bc16f164e7d7535e71b184e00dd48c29
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-23 17:23:35 +0530

    Update and rename color_code.py to color_code_Initializer.py

---
+++ color_code_Initializer.py
@@ -0,0 +1,9 @@
+
+
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+def color_pair_to_string(major_color, minor_color): #Returns the color pair as a formatted string.
+    return f'{major_color} {minor_color}'
+
+
