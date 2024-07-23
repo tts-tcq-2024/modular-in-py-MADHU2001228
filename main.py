910b570ac0bf9908d3606a9225259dc0207d3fd0
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-07-23 17:41:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+if __name__ == '__main__':
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('All tests passed!')
